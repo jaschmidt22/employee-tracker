fa6d257d535c4cefe93b5d138f8b44117954238f
jaschmidt22 <146290812+jaschmidt22@users.noreply.github.com>
2024-01-20 21:12:36 -0700

    seeds started server

--- db/schema.sql
+++ db/schema.sql
@@ -4,44 +4,14 @@ CREATE DATABASE employeetracker_db;
 
 USE employeetracker_db;
 
--- department
-
---     id: INT PRIMARY KEY
-
---     name: VARCHAR(30) to hold department name
-
-CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, department_name VARCHAR(30) -- to hold department name
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, department_name VARCHAR(30)
 );
--- role
-
---     id: INT PRIMARY KEY
-
---     title: VARCHAR(30) to hold role title
-
---     salary: DECIMAL to hold role salary
-
---     department_id: INT to hold reference to department role belongs to
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, title VARCHAR(30), -- to hold role title
-    salary DECIMAL, -- to hold role salary
-    department_id INT, -- to hold reference to department role belongs to
-    FOREIGN KEY (department_id) REFERENCES department (id) --values in the department_id column of the "roles" table should match the values in the id column of the "department" table
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, title VARCHAR(30), salary DECIMAL(10, 2), department_id INT, FOREIGN KEY (department_id) REFERENCES departments (id) ON DELETE SET NULL
 );
--- employee
 
---     id: INT PRIMARY KEY
-
---     first_name: VARCHAR(30) to hold employee first name
-
---     last_name: VARCHAR(30) to hold employee last name
-
---     role_id: INT to hold reference to employee role
 CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(30), -- to hold employee first name
-    last_name VARCHAR(30), -- to hold employee last name
-    role_id INT, -- to hold reference to employee role
-    FOREIGN KEY (role_id) REFERENCES roles (id) -- the values in the role_id column of the "employees" table should match the values in the id column of the "roles" table
-    manager_id INT -- to hold reference to another employee that is manager of the current employee -- may be null if the employee has no manager
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(30), last_name VARCHAR(30), role_id INT, manager_id INT
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,34 @@
--- departments (department_name)
+INSERT INTO
+    departments (department_name)
+VALUES ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
--- roles (title, salary, department_id)
+INSERT INTO
+    roles (title, salary, department_id)
+VALUES ('Sales Manager', 120000, 1),
+    ('Salesperson', 100000, 1),
+    ('Lead Engineer', 150000, 2),
+    (
+        'Software Engineer', 120000, 2
+    ),
+    ('Account Manager', 150000, 3),
+    ('Accountant', 120000, 3),
+    (
+        'Legal Department Manager', 300000, 4
+    ),
+    ('Lawyer', 250000, 4);
 
--- employees ( first_name, last_name, role_id, manager_id)
+INSERT INTO
+    employees (
+        first_name, last_name, role_id, manager_id
+    )
+VALUES ('Tristan', 'Schmidt', 1, NULL),
+    ('Colin', 'Schmidt', 2, 1),
+    ('Karina', 'Schmidt', 3, NULL),
+    ('Jessica', 'Schmidt', 4, 3),
+    ('Lea', 'Villaverde', 5, NULL),
+    ('Mary', 'Jibben', 6, 5),
+    ('Marie', 'Green', 7, NULL),
+    ('Alexa', 'Bellamy', 8, 7);
